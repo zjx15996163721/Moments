6bf497c42dbbc32d15d0a7f4777f411291807a61
zjx <1735429225@qq.com>
2021-05-10 17:27:04 +0800

    new

--- Moments/src/demo.java
+++ Moments/src/demo.java
@@ -1,7 +1,5 @@
 public class demo {
     public static void main(String[] args) {
         System.out.println("aaa");
-        System.out.println("bbb");
-        System.out.println("ccc");
     }
 }
