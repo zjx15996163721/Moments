d5b2ea4019133f8958f1f05a91a1ae6db1a319cf
zjx <1735429225@qq.com>
2021-05-11 09:29:41 +0800

    new

--- Moments/src/demo.java
+++ Moments/src/demo.java
@@ -1,6 +1,7 @@
 public class demo {
     public static void main(String[] args) {
         System.out.println("aaa");
-        System.out.println("aaa");
+        System.out.println("bbb");
+        System.out.println("ccc");
     }
 }
